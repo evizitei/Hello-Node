b7ab21ca0292e66974099e2bcf89d3f879960e64
Ethan Vizitei <ethan.vizitei@gmail.com>
2015-07-28 17:09:16 -0600

    add number summing endpoint

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+app.use(bodyParser.json());
+
 app.get('/', function(req, res){
   res.send('Hello world\n');
 });
+
+app.post('/sum', function(req, res){
+  numbers = req.body.numbers;
+  sum = numbers.reduce(function(s, val){
+    return s + val;
+  }, 0);
+  res.json({ solution: sum });
+});
+
 app.listen(8080);
-console.log("Express server listening on port %d", 8080)
+console.log("Express server listening on port %d", 8080);
